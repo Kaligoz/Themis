5067480142bb6b1c02cd13afce3e9a0539223ae2
unknown <glebkalinichenkolol@gmail.com>
2026-01-05 19:25:35 +0200

    first commit

---
+++ next-i18next.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from 'next-i18next';
+
+const nextI18NextConfig: UserConfig = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ua'],
+  },
+};
+
+export default nextI18NextConfig;
